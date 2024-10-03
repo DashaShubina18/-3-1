0c8b3a4f70be76bc310741b50197f401217e22ea
Даша <@DESKTOP-L6GFU94>
2024-10-03 13:16:19 +0300

    Add project files.

---
+++ лабораторна 3 завдання 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лабораторна_3_завдання_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Input n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            int i = 1;
+            double sum = 0;
+            while (i < n)
+            {
+                sum += Math.Pow(-1, i) * (2 * i - 1) / Math.Pow(x, i);
+                i++;
+            }
+            Console.WriteLine($"Сума s = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
